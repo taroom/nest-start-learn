475a941220786f783d0737ae9229e8953c456b1d
Sultan Room <sultanroomcode@gmail.com>
2024-06-25 21:49:24 +0700

    install mock node

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -12,7 +13,19 @@ describe('UserController', () => {
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should can say hello', async () => {
+    const response = await controller.asyncSayHello();
+    expect(response).toBe('Hallo Kawan saya menggunakan async');
+  });
+
+  it('should read view html with node mock', async () => {
+    const response = httpMock.createResponse();
+    controller.viewHello('Taroom', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      name: 'Taroom',
+      title: 'Nest with HTML Render View'
+    })
   });
 });
