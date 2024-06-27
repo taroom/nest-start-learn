e3688da323cea31e24107416598563800cad222f
Sultan Room <sultanroomcode@gmail.com>
2024-06-27 11:27:12 +0700

    add user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
-  controllers: [UserController]
+  controllers: [UserController],
+  providers: [UserService]
+  // service harus diregistrasikan disini jika tidak dia tidak akan terpanggil walau sudah kita buat
 })
-export class UserModule {}
+export class UserModule { }
